4bba382c044e75a47688c3f1203dfe94191aa06c
IorYohanna <yorakotosandratana@gmail.com>
2025-10-20 11:35:47 +0300

    initialized project

---
+++ src/test/java/com/example/dfcr/DfcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dfcr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
