70d94f135d4aa9d0a586f267a69c276cbcb75fa5
HerizoRand <hontyherizorandrianarisoa@yahoo.com>
2025-10-26 17:46:31 +0300

    Test PreAuthorize with function

--- src/main/java/com/example/Auth/controller/UserController.java
+++ src/main/java/com/example/Auth/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.Auth.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,5 +32,11 @@ public class UserController {
     public ResponseEntity<List<User>> allUsers() {
         List <User> users = userService.allUsers();
         return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/hello")
+    @PreAuthorize("hasRole('ROLE_chef')")
+    public String hello() {
+        return "Hello, Authenticated User!";
     }
 }
